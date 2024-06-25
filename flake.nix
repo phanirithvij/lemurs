8f110995941c56e923801af62ddf26aa53e4cd8c
phanirithvij <phanirithvij2000@gmail.com>
2024-06-25 12:21:42 +0530

    Format and Remove Settings Thing
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- flake.nix
+++ flake.nix
@@ -9,152 +9,155 @@
   };
 
   outputs = { self, nixpkgs, utils, rust-overlay }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        packageName = "lemurs";
-        
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlays.default ];
-        };
-  
-        rustToolchain = pkgs.rust-bin.stable.latest.default;
-  
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustToolchain;
-          rustc = rustToolchain;
-        };
-      in rec {
-        packages.lemurs = rustPlatform.buildRustPackage {
-          name = packageName;
-          src = ./.;
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          packageName = "lemurs";
 
-          postPatch = ''
-            substituteInPlace ./extra/config.toml --replace "/bin/sh" "${pkgs.bash}/bin/bash"
-            substituteInPlace ./extra/config.toml --replace "/usr/bin/X" "${pkgs.xorg.xorgserver}/bin/X"
-            substituteInPlace ./extra/config.toml --replace "/usr/bin/xauth" "${pkgs.xorg.xauth}/bin/xauth"
-          '';
-
-          buildInputs = [
-            pkgs.linux-pam
-          ];
-          
-          cargoHash = "sha256-rJLHfedg4y8cZH77AEA4AjE0TvWf9tdSjKiHZfvW+gw=";  
-        };
-        packages.default = packages.lemurs;
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            linux-pam
-          ];
-        };
-      }
-  ) // {
-    nixosModules.default = {
-      pkgs,
-      lib,
-      config,
-      ...
-    }: let
-      sessionData = config.services.xserver.displayManager.sessionData;
-    in {
-      options.services.lemurs = rec {
-        enable = lib.mkEnableOption "Enable the Lemurs Display Manager";
-
-        x11.enable = lib.mkEnableOption "Enable the X11 part of the Lemurs Display Manager";
-        wayland.enable = lib.mkEnableOption "Enable the Wayland part of the Lemurs Display Manager";
-        
-        tty = lib.mkOption {
-          type = lib.types.str;
-          default = "tty2";
-        };
-
-
-        settings = {
-          x11 = {
-            xauth = lib.mkOption {
-              type = lib.types.nullOr lib.types.package;
-              default = if x11.enable then pkgs.xorg.xauth else null;
-            };
-
-            xorgserver = lib.mkOption {
-              type = lib.types.nullOr lib.types.package;
-              default = if x11.enable then pkgs.xorg.xorgserver else null;
-            };
-            
-            xsessions = lib.mkOption {
-              type = lib.types.path;
-              default = "${sessionData}/share/xsessions";
-            };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
           };
 
-          wayland = {
-            wayland-sessions = lib.mkOption {
-              type = lib.types.path;
-              default = "${sessionData}/share/wayland-sessions";
-            };
+          rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
           };
-        };
-      };
+        in
+        rec {
+          packages.lemurs = rustPlatform.buildRustPackage {
+            name = packageName;
+            src = ./.;
 
-      config = let
-        cfg = config.services.lemurs;
-      in lib.mkIf cfg.enable {
-        nixpkgs.overlays = [
-          (final: prev: { lemurs = self.packages.x86_64-linux.default; })
-        ];
-
-        security.pam.services.lemurs = {
-          allowNullPassword = true;
-          startSession = true;
-          setLoginUid = false;
-          enableGnomeKeyring = lib.mkDefault config.services.gnome.gnome-keyring.enable;
-        };
-
-        systemd.services."autovt@${cfg.tty}".enable = false;
-
-        systemd.services.lemurs = {
-          aliases = [ "display-manager.service" ];
-          
-          unitConfig = {
-            Wants = [
-              "systemd-user-sessions.service"
-            ];
-
-            After = [
-              "systemd-user-sessions.service"
-              "plymouth-quit-wait.service"
-              "getty@${cfg.tty}.service"
-            ];
-
-            Conflicts = [
-              "getty@${cfg.tty}.service"
-            ];
-          };
-
-          serviceConfig = {
-            ExecStart = ''
-              ${pkgs.lemurs}/bin/lemurs                      \
-                --xsessions  ${cfg.settings.x11.xsessions}            \
-                --wlsessions ${cfg.settings.wayland.wayland-sessions}
+            postPatch = ''
+              substituteInPlace ./extra/config.toml --replace "/bin/sh" "${pkgs.bash}/bin/bash"
+              substituteInPlace ./extra/config.toml --replace "/usr/bin/X" "${pkgs.xorg.xorgserver}/bin/X"
+              substituteInPlace ./extra/config.toml --replace "/usr/bin/xauth" "${pkgs.xorg.xauth}/bin/xauth"
             '';
 
-            StandardInput = "tty";
-            TTYPath = "/dev/${cfg.tty}";
-            TTYReset = "yes";
-            TTYVHangup = "yes";
+            buildInputs = [
+              pkgs.linux-pam
+            ];
 
-            Type = "idle";
+            cargoHash = "sha256-rJLHfedg4y8cZH77AEA4AjE0TvWf9tdSjKiHZfvW+gw=";
+          };
+          packages.default = packages.lemurs;
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              linux-pam
+            ];
+          };
+        }
+      ) // {
+      nixosModules.default =
+        { pkgs
+        , lib
+        , config
+        , ...
+        }:
+        let
+          sessionData = config.services.xserver.displayManager.sessionData;
+        in
+        {
+          options.services.lemurs = rec {
+            enable = lib.mkEnableOption "Enable the Lemurs Display Manager";
+
+            x11.enable = lib.mkEnableOption "Enable the X11 part of the Lemurs Display Manager";
+            wayland.enable = lib.mkEnableOption "Enable the Wayland part of the Lemurs Display Manager";
+
+            tty = lib.mkOption {
+              type = lib.types.str;
+              default = "tty2";
+            };
+
+            x11 = {
+              xauth = lib.mkOption {
+                type = lib.types.nullOr lib.types.package;
+                default = if x11.enable then pkgs.xorg.xauth else null;
+              };
+
+              xorgserver = lib.mkOption {
+                type = lib.types.nullOr lib.types.package;
+                default = if x11.enable then pkgs.xorg.xorgserver else null;
+              };
+
+              xsessions = lib.mkOption {
+                type = lib.types.path;
+                default = "${sessionData}/share/xsessions";
+              };
+            };
+
+            wayland = {
+              wayland-sessions = lib.mkOption {
+                type = lib.types.path;
+                default = "${sessionData}/share/wayland-sessions";
+              };
+            };
           };
 
-          restartIfChanged = false;
+          config =
+            let
+              cfg = config.services.lemurs;
+            in
+            lib.mkIf cfg.enable {
+              nixpkgs.overlays = [
+                (final: prev: { lemurs = self.packages.x86_64-linux.default; })
+              ];
 
-          wantedBy = [ "graphical.target" ];
+              security.pam.services.lemurs = {
+                allowNullPassword = true;
+                startSession = true;
+                setLoginUid = false;
+                enableGnomeKeyring = lib.mkDefault config.services.gnome.gnome-keyring.enable;
+              };
+
+              systemd.services."autovt@${cfg.tty}".enable = false;
+
+              systemd.services.lemurs = {
+                aliases = [ "display-manager.service" ];
+
+                unitConfig = {
+                  Wants = [
+                    "systemd-user-sessions.service"
+                  ];
+
+                  After = [
+                    "systemd-user-sessions.service"
+                    "plymouth-quit-wait.service"
+                    "getty@${cfg.tty}.service"
+                  ];
+
+                  Conflicts = [
+                    "getty@${cfg.tty}.service"
+                  ];
+                };
+
+                serviceConfig = {
+                  ExecStart = ''
+                    ${pkgs.lemurs}/bin/lemurs                      \
+                      --xsessions  ${cfg.settings.x11.xsessions}            \
+                      --wlsessions ${cfg.settings.wayland.wayland-sessions}
+                  '';
+
+                  StandardInput = "tty";
+                  TTYPath = "/dev/${cfg.tty}";
+                  TTYReset = "yes";
+                  TTYVHangup = "yes";
+
+                  Type = "idle";
+                };
+
+                restartIfChanged = false;
+
+                wantedBy = [ "graphical.target" ];
+              };
+
+              systemd.defaultUnit = "graphical.target";
+
+            };
         };
-
-        systemd.defaultUnit = "graphical.target";
-
-      };
     };
-  };
 }
